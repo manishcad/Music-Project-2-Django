5b1597b5a15584eca29fcb15c257d0f9bb2d5479
Manish Chand <manishtochand@gmail.com>
2022-08-23 10:48:42 +0530

    Add files via upload

--- Music/settings.py
+++ Music/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-_v_wr^ejv=#m+#f6t#$)l^&3f=^m-p15m$o7@(4@b_nblp+4ah
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['music-legend.herokuapp.com,', '127.0.0.1']
+ALLOWED_HOSTS = ['music-legend.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
